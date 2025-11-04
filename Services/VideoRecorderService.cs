9e9bee09f4304155ad97941dbf9465bbe7e6d8b5
YuSeungTae <tmdxo1102@gmail.com>
2025-11-04 14:21:33 +0900

    fix bbox modify

--- Services/VideoRecorderService.cs
+++ Services/VideoRecorderService.cs
@@ -84,7 +84,8 @@ namespace Operation_Control_System.Services
 
             try
             {
-                var merged = await Task.Run(() => RenderFrameWithBBoxes(frame, boxes));
+                var snapshot = boxes?.ToList();
+                var merged = await Task.Run(() => RenderFrameWithBBoxes(frame, snapshot));
 
                 int stride = merged.PixelWidth * (merged.Format.BitsPerPixel / 8);
                 byte[] pixels = new byte[stride * merged.PixelHeight];
